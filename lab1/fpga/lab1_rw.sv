4e39084ec8c4daa86221923560574b6852ea4d1e
Rai <andre.wandeler@me.com>
2026-09-06 17:51:51 -0700

    Re-write oscillator to be simple counter with duty

--- lab1/fpga/lab1_rw.sv
+++ lab1/fpga/lab1_rw.sv
@@ -5,15 +5,15 @@ module lab1_rw(input logic reset,
 	// input switches are pull-up, hence extra NOT gates
     logic int_osc;
     logic [6:0] seg;
-    logic [31:0] counter_blink_led;
+    logic signed [24:0] counter_blink_led;
 
     sevensegment_hex display(~switches, seg);
     HSOSC hf_osc(.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
-    counter_pn #(32,32'd215) blink_led(int_osc, ~reset, 1'b1, counter_blink_led);
+    oscillator_vduty #(25,-25'd10000000,25'd9999999) blink_led(int_osc, ~reset, 1'b1, counter_blink_led);
 
     assign leds[0] = ~switches[0] ^ ~switches[1];
     assign leds[1] = ~switches[2] & ~switches[3];
-    assign leds[2] = counter_blink_led[31];
+    assign leds[2] = ~counter_blink_led[24]; // sign bit controls LED
     assign segments = ~seg;
 
 endmodule
